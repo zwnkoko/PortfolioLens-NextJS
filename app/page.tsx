ae0d6d9552f064dd32927c7a252ef3c8403491d8
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-12 21:43:30 +0000

    init home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
-import Image from "next/image";
+import Button from "./components/Button";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col justify-center size-full items-center">
+      <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 text-center text-2xl md:text-6xl"> 
+        Minimalistic Portfolio Tracker for Retail Investors 
+      </p>
+
+      <Button label="Get Started" id="get-started" animation={true} />
+    
       
+
     </div>
   );
 }
