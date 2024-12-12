1aa518fdb4d899cb981f335b9650cc149d6ea7e2
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-12 21:42:07 +0000

    add btn component

---
+++ app/components/Button.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { ReactNode } from "react";
+import { GestureWrapper, GestureWrapperProps } from "./MotionWrapper";
+
+interface ButtonProps extends GestureWrapperProps{
+    label: string;
+    id: string;
+    prefix?: ReactNode,
+    suffix?: ReactNode,
+    onClick?: () => void;
+    variant?: "primary" | "secondary" | "tertiary";
+    animation?: boolean,
+    href?: string,
+  }
+  
+const Button = ({ label, id, prefix, suffix, onClick, variant = "primary", animation, hoverScale, tapScale, href }: ButtonProps) => {
+    const variantClasses = 
+    {
+        primary: "bg-primary hover:bg-primary-hover active:bg-primary-active",
+        secondary:"bg-secondary hover:bg-secondary-hover active:bg-secondary-active",
+        tertiary: "bg-tertiary hover:bg-tertiary-hover active:bg-tertiary-active",
+    };
+
+    const handleClick = () => {
+        if(onClick){
+            // to be implemented
+        }
+        if(href){
+            // to be implemented
+        }
+    }
+
+    const buttonElement = (
+        <button
+            type="button"
+            className={`font-bold rounded-md py-3 px-4 ${variantClasses[variant]} text-white text-sm lg:text-base size-full`}
+            id={id}
+            onClick={handleClick}
+        >
+            <div className="flex justify-around">
+                {prefix}
+                <p>{label}</p>
+                {suffix}
+            </div>
+
+        </button>
+    )
+    
+    return animation ?
+    (
+        <GestureWrapper hoverScale={hoverScale} tapScale={tapScale}>
+            {buttonElement}
+        </GestureWrapper>
+    ) : 
+    (
+        <div>
+            {buttonElement}
+        </div>
+    )
+};
+
+export default Button;
--- app/components/MotionWrapper.tsx
+++ app/components/MotionWrapper.tsx
@@ -3,13 +3,13 @@
 import { motion } from "motion/react"
 import { ReactNode } from "react";
 
-interface Props{
-    children: ReactNode,
+export interface GestureWrapperProps {
+    children?: ReactNode,
     hoverScale?: number,
     tapScale?: number,
 }
 
-export const GestureWrapper = ({ children, hoverScale=1.2, tapScale=1 } : Props) => {
+export const GestureWrapper = ({ children, hoverScale=1.2, tapScale=1 } : GestureWrapperProps) => {
   return (
     <motion.div whileHover={{scale: hoverScale}} whileTap={{scale: tapScale}}>
         {children}
