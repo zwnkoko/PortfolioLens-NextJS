4abb7ff851ffba0eda5a43eafe10c0387c42b2b0
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-12 18:52:26 +0000

    set up theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: 'selector',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,9 +10,40 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+
+        // Button Colors 
+        primary: {
+          DEFAULT: "var(--color-primary-default)",
+          hover: "var(--color-primary-hover)",
+          active: "var(--color-primary-active)",
+        },
+        secondary: {
+          DEFAULT: "var(--color-secondary-default)",
+          hover: "var(--color-secondary-hover)",
+          active: "var(--color-secondary-active)",
+        },
+        tertiary: {
+          DEFAULT: "var(--color-tertiary-default)",
+          hover: "var(--color-tertiary-hover)",
+          active: "var(--color-tertiary-active)",
+        },
+        
       },
+      keyframes:{
+        scroll:{
+          '0%': { 
+            transform: 'translateX(0)', 
+            '-webkit-transform': 'translateX(0)',
+          },
+          '100%': { 
+            transform: 'translateX(-50%)',
+            '-webkit-transform': 'translateX(-50%)',
+          },
+        }
+      },
+      animation:{
+        scroll: 'scroll 20s linear infinite 1s',
+      }
     },
   },
   plugins: [],
