f24a5f2481b6654a8c637beb2cdaa0700d27aa6a
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-12 21:42:32 +0000

    change layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
+import NavBar from "./components/NavBar";
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'],
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "Portfolio Lens",
@@ -12,17 +20,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className="antialiased">
-        <header>
-          {/* Navigation Bar */}
-        </header>
-        <main>
-          {children}
-        </main>
-        <footer>
-          {/* Footer */}
-        </footer>
+    <html lang="en" className={roboto.className} suppressHydrationWarning>
+      <body className="antialiased grid grid-rows-12 min-h-dvh lg:max-w-7xl mx-auto">
+        <ThemeProvider attribute="class">
+          <header>
+            <NavBar />
+          </header>
+          <main className="row-span-10">
+            {children}
+          </main>
+          <footer className="flex items-center justify-center">
+            <p className="text-center">
+              Lorem ipsum dolor sit amet
+            </p>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
   );
