1fb13e08821d35555e35785d656f47aa4c19ba4a
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-15 14:44:30 +0000

    add github button

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,22 @@ import Button from "./components/Button";
 
 export default function Home() {
   return (
-    <div className="flex flex-col justify-center size-full items-center">
-      <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 text-center text-2xl md:text-6xl"> 
-        Minimalistic Portfolio Tracker for Retail Investors 
-      </p>
+    <div className="flex flex-col justify-center size-full items-center space-y-8 md:space-y-14">
+      <div className="text-center space-y-8">
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-pink-400 text-center text-2xl md:text-5xl"> 
+          Minimalistic Portfolio Tracker for Retail Investors 
+        </p>
+        <p className="text-gray-600 dark:text-slate-300 text-sm md:text-lg">
+          Effortlessly manage your investments with <br className="md:hidden"/> a sleek, <span className="whitespace-nowrap">user-friendly</span> interface.
+        </p>
+      </div>
 
-      <Button label="Get Started" id="get-started" animation={true} />
-    
+      <div className="flex space-x-6">
+        <Button label="Github" id="github-repo" animation={true} variant="tertiary"/>
+        <Button label="Get Started" id="get-started" animation={true} />
+      </div>
       
-
+    
     </div>
   );
 }
