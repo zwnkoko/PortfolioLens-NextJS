ba893c376e05bfd0dd0d9aa3c4894b8f69704d55
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-16 20:34:20 +0000

    add pading

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,10 +23,10 @@ export default function RootLayout({
     <html lang="en" className={roboto.className} suppressHydrationWarning>
       <body className="antialiased grid grid-rows-[auto_1fr_auto] min-h-dvh md:max-w-7xl mx-auto px-global-x py-4">
         <ThemeProvider attribute="class">
-          <header>
+          <header className="pb-4">
             <NavBar />
           </header>
-          <main>
+          <main className="pb-4">
             {children}
           </main>
           <footer className="flex items-center justify-center">
