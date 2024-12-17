53ca8f9aaa491c28e623bccec1e5363dff72d016
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2024-12-17 08:27:20 +0800

    remove unused imports

--- app/components/Drawer.tsx
+++ app/components/Drawer.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react"
 import Link from "next/link";
-import { motion } from "motion/react";
+
 
 const Drawer = () => {
     const [ openDrawer, setOpenDrawer ] = useState(false);
--- app/dashboard/@chart/page.tsx
+++ app/dashboard/@chart/page.tsx
@@ -1,8 +1,4 @@
-const holdings = {
-    MU: 2, 
-    AAPL: 3, 
-    GOOGL: 5,
-}
+
 export default function Chart(){
     return(
         <div>
