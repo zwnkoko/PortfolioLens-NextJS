92241613223b7b98a5b4aac2188f1330cc0b4731
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2025-01-26 04:56:10 +0000

    add external links

---
+++ app/constants.ts
@@ -0,0 +1,13 @@
+export const EXTERNAL_LINKS = {
+  // Repo link
+  github_repo_link: "https://github.com/zwnkoko/PortfolioLens-NextJS",
+
+  // MIT License
+  license_link:
+    "https://github.com/zwnkoko/PortfolioLens-NextJS/blob/main/LICENSE",
+
+  // Footer links
+  github_profile: "https://github.com/zwnkoko",
+  linkedin_profile: "https://www.linkedin.com/in/zwnkoko",
+  email: "mailto:zwnkoko@gmail.com",
+};
