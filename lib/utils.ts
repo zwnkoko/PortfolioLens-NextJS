471ec567499294aac4496524639e5a47b03290b9
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2025-02-04 16:53:37 +0000

    add logging wrapper utility for development

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const logDevError = (
+  message: unknown,
+  type: "log" | "error" | "warn" | "info" = "log",
+) => {
+  if (process.env.NODE_ENV === "development") {
+    console[type](message);
+  }
+};
