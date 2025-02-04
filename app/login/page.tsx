f3c512bff2c87b88bce1845f1ce368a690ac8f9b
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2025-02-04 16:53:47 +0000

    refactor authentication logic by creating a custom hook and simplifying login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { auth, provider } from "@/lib/firebaseClient";
-import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { useAuth } from "@/hooks/use-auth";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import {
@@ -12,36 +10,9 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        console.log("User is signed in");
-        router.push("/dashboard");
-      } else {
-        console.log("User is signed out");
-      }
-    });
-
-    console.log("executing");
-    return () => unsubscribe();
-  }, []);
-  const handleGoogleSignIn = async () => {
-    try {
-      provider.setCustomParameters({
-        prompt: "select_account",
-      });
-
-      const result = await signInWithPopup(auth, provider);
-      console.log(result);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+  const { signInWithGoogle } = useAuth();
 
   return (
     <div className="flex h-full items-center justify-center">
@@ -53,7 +24,7 @@ export default function LoginPage() {
           </CardDescription>
         </CardHeader>
         <CardContent>
-          <Button onClick={handleGoogleSignIn} className="w-full">
+          <Button onClick={signInWithGoogle} className="w-full">
             <Image
               src="/logo/google.svg"
               alt="Google Logo"
---
+++ hooks/use-auth.ts
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { auth, provider } from "@/lib/firebaseClient";
+import { User, signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { useRouter } from "next/navigation";
+import { logDevError } from "@/lib/utils";
+
+export function useAuth() {
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      if (user) {
+        router.push("/dashboard");
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const signInWithGoogle = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      return result;
+    } catch (error) {
+      logDevError(error);
+    }
+  };
+
+  return {
+    user,
+    signInWithGoogle,
+  };
+}
