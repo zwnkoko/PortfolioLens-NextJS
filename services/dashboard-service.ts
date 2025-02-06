1c640a0eea137efcd24e3045efa77b299fd2f5ab
zwnkoko <123788806+zwnkoko@users.noreply.github.com>
2025-02-07 04:55:45 +0800

    replace logDevError with logDev for improved clarity in error logging

--- services/dashboard-service.ts
+++ services/dashboard-service.ts
@@ -1,13 +1,13 @@
 import { db } from "@/lib/firebaseClient";
 import { doc, getDoc } from "firebase/firestore/lite";
-import { logDevError } from "@/lib/utils";
+import { logDev } from "@/lib/utils";
 
 export async function fetchUserHoldings(uid: string) {
   const docRef = doc(db, "holdings", uid);
   const docSnap = await getDoc(docRef);
 
   if (!docSnap.exists()) {
-    logDevError("No document found for user's holdings");
+    logDev("No document found for user's holdings");
     throw new Error("No record exists");
   }
 
